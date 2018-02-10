4e66acd7129007bd7011754b096786842ef4f02e
Slava7713 <slava7713@yandex.ru>
2018-02-10 19:32:28 +0300

    adding more logging

--- test_student_update.py
+++ test_student_update.py
@@ -1,4 +1,9 @@
 import logging
+import sys
+log = logging.getLogger(__name__)
+out_hdlr = logging.StreamHandler(sys.stdout)
+log.addHandler(out_hdlr)
+log.setLevel(logging.INFO)
 
 logging.debug('debug')
 logging.info('info')
