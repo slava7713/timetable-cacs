c32a7f333053119f8eac14dd5543f0fc6bd2526c
Slava7713 <slava7713@yandex.ru>
2018-02-10 19:30:06 +0300

    adding more logging

--- update.py
+++ update.py
@@ -1,6 +1,5 @@
 from database_interaction import purge_old, get_all_db, update_file
 from calendar_creation import create_calendar
-from requests.exceptions import ReadTimeout
 import logging
 # Firstly, remove old subscriptions
 purge_old()
@@ -11,13 +10,15 @@ items = get_all_db()
 for selst in items[0]:
     try:
         update_file(selst, create_calendar(selst, prof=False), prof=False)
-    except ReadTimeout:
+    except Exception as exc:
         logging.error('failed update for student %d' % selst)
+        logging.error(exc)
         pass
 
 for prr in items[1]:
     try:
         update_file(prr, create_calendar(prr, prof=True), prof=True)
-    except ReadTimeout:
+    except Exception as exc:
         logging.error('failed update for prof %d' % prr)
+        logging.error(exc)
         pass
