a45786321582c0f7bb4956b4b48ea9d56944a9e5
slava <slava7713@yandex.ru>
2023-02-17 00:34:18 +0400

    Removed worker, now updates are all run via scheduler and update.py

--- update.py
+++ update.py
@@ -1,9 +1,10 @@
 from database_interaction import purge_old, get_all_db, list_data
 from send_error_report import send_email
 from update_helper import update_individual_prof, update_individual_student
-from rq import Queue
-from worker import conn
 import logging
+import os
+import redis
+from rq import Worker, Queue, Connection
 log = logging.getLogger('gunicorn.error')
 
 # Firstly, remove old subscriptions
@@ -12,12 +13,24 @@ purge_old()
 # Get all items for update
 items = get_all_db()
 
+
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+
+conn = redis.from_url(redis_url)
+
 # Start the queue
 q = Queue(connection=conn)
 for item in items[0]:
     q.enqueue(update_individual_student, item)
 for item in items[1]:
     q.enqueue(update_individual_prof, item)
+
+# Start the worker
+listen = ['high', 'default', 'low']
+with Connection(conn):
+    worker = Worker(map(Queue, listen))
+    worker.work()
+
 
 # End by counting total student errors and if the number is high enough trigger a warning
 
